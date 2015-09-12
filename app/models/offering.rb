a932711140d522c9129b336af51b7ea8e6be9b4d
Bradley Berger <hippy.berger@gmail.com>
2015-09-11 21:28:03 -0600

    Reformat Admin/offerings/index to follow parent-child hierarchy when displaying offerings.

--- app/models/offering.rb
+++ app/models/offering.rb
@@ -1,5 +1,4 @@
 class Offering < ActiveRecord::Base
-
   acts_as_tree
 
   has_many :offering_user_povs, dependent: :destroy
@@ -8,19 +7,6 @@ class Offering < ActiveRecord::Base
 
   def self.arrange_by_position
     order('display_position ASC')
-  end
-
-  # both methods below are unnecessary and should be implemented with 'acts_as_tree' gem
-  def self.find_parent(child)
-    if child.parent_id.nil?
-      'parent offering'
-    else
-      find(child.parent_id)
-    end
-  end
-
-  def self.find_children
-    Offering.all.select { |offering| offering.parent_id == self.id }
   end
 
   def visible?
