93918c580cd104d337f500f90e7bd396e0c26697
Allen Krauskopf <alkrauskopf@gmail.com>
2015-09-23 16:05:46 -0600

    Temp Commit to figure out Heroku error

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,8 @@ class WelcomeController < ApplicationController
 
   def index
   #  load_team
-  @welcomes = Offering.welcomes.active
+#  @welcomes = Offering.welcomes.active
+    @welcomes = []
   @users = User.core_team
   @pov_users = User.with_povs
   end
