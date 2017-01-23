90ecffeb0f9552ad43d8a9a20f1f6bf400442049
Allen Krauskopf <alkrauskopf@gmail.com>
2017-01-23 12:33:37 -0700

    Add Background Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
 
   def offering_palette(offering)
-    background = offering.ancestor.panel_color[0] == '#' ? offering.ancestor.panel_color : '#797980'
+    if !offering.nil?
+      background = offering.ancestor.panel_color[0] == '#' ? offering.ancestor.panel_color : '#797980'
+    else
+      background = '#ffffff'
+    end
     return background
   end
 
