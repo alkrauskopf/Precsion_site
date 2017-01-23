c7304986efcabc2c9ab885e90b01b62c77e71d2b
Allen Krauskopf <alkrauskopf@gmail.com>
2017-01-23 12:51:26 -0700

    does helper work in prod?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
 
   def offering_palette(offering)
-    if !offering.nil?
+ #   if !offering.nil?
+    if false
       background = offering.panel_color.include?('#') ? offering.panel_color : '#797980'
     else
       background = '#ffffff'
