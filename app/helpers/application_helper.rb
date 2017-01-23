734282838a7961cee6243ac4aad4eff8c2790c97
Allen Krauskopf <alkrauskopf@gmail.com>
2017-01-23 12:45:17 -0700

    still figuring the helper problem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,10 @@ module ApplicationHelper
 
   def offering_palette(offering)
     if !offering.nil?
-      background = offering.ancestor.panel_color.include?('#') ? offering.ancestor.panel_color : '#797980'
+      background = offering.panel_color.include?('#') ? offering.panel_color : '#797980'
     else
       background = '#ffffff'
     end
     return background
   end
-
 end
