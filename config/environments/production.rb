7b935bf016248d5fb50c98adffd745fb0f56b13a
Allen Krauskopf <alkrauskopf@gmail.com>
2017-03-01 11:53:24 -0700

    Fix Email Notifier route to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,10 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+
+
+  config.action_mailer.default_url_options = {    host: "precisionschoolimprovement.com"}
+
   # Don't care if the mailer can't send.
   # config.action_mailer.raise_delivery_errors = false
 
