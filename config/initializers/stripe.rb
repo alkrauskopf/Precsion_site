499fd57d6b6b5879533d06d426b21eeead1b1ca7
Allen Krauskopf <alkrauskopf@gmail.com>
2017-03-18 08:36:23 -0600

    Stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-#  Stripe.api_key = Rails.application.secrets.stripe_secret_key
-#  raise "Missing Stripe API Key" unless Stripe.api_key
-#  STRIPE_JS_HOST = "https://js.stripe.com".freeze unless defined? STRIPE_JS_HOST
-#  STRIPE_JS_FILE = Rails.env.development? ? "stripe-debug.js" : ""
+  Stripe.api_key = Rails.application.secrets.stripe_secret_key
+  raise "Missing Stripe API Key" unless Stripe.api_key
+  STRIPE_JS_HOST = "https://js.stripe.com".freeze unless defined? STRIPE_JS_HOST
+  STRIPE_JS_FILE = Rails.env.development? ? "stripe-debug.js" : ""
